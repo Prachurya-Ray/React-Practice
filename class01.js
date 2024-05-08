92b8f9f9f680867f11d47b7088e5c205d3d370bd
Prachurya Ray <prachuryaray@gmail.com>
2024-05-08 12:20:21 +0530

    box made

--- class01.js
+++ class01.js
@@ -1,15 +1,22 @@
-
 let head = React.createElement("h1",{
     id:"firstLine"
 }, "Hello 1");
 
 let head2 = React.createElement("h2",{
-    id:"firstLine2"
+    id:"firstLine2",
 }, "Hello 2");
+
+let h1 = React.createElement("div",{
+    className:"blue",
+}, head);
+
+let h2 = React.createElement("div",{
+    className:"red",
+}, head2);
 
 let headDiv = React.createElement("div",{
     id:"head"
-}, [head, head2]);
+}, [h1, h2]);
 
 let root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(headDiv);
+root.render(headDiv);   
